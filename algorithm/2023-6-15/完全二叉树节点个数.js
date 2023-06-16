84f134efb70075662c6160caf79286e3b7872746
zhaohao <zhaohao@bizgo.com>
2023-06-16 15:44:31 +0800

    feat: 算法2023-6-15 答案

--- algorithm/2023-6-15/完全二叉树节点个数.js
+++ algorithm/2023-6-15/完全二叉树节点个数.js
@@ -14,7 +14,7 @@
  * @property {Object} right
  */
 /**
- * @param {TreeNode} root
+ * @param {TreeNode | null} root
  * @return {number}
  */
 function countNodes(root) {
@@ -37,3 +37,46 @@ function countLevel(root){
     }
     return level;
 }
+
+/**
+ * Test
+ */
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
+// [1,2,3,4,5,6]
+class CBTInserter {
+    list = []
+    idx = 0
+    constructor(root) {
+        this.list.push(root)
+        let cur = 0
+        while (cur < this.list.length) {
+            const node = this.list[cur]
+            if (node.left != null) this.list.push(node.left)
+            if (node.right != null) this.list.push(node.right)
+            cur++
+        }
+    }
+    insert(val) {
+        const node = new TreeNode(val)
+        while (this.list[this.idx].left != null && this.list[this.idx].right != null) this.idx++
+        const fa = this.list[this.idx]
+        if (fa.left == null) fa.left = node
+        else if (fa.right == null) fa.right = node
+        this.list.push(node)
+        return fa.val
+    }
+    get_root(){
+        return this.list[0]
+    }
+}
+const arr = [1,2,3,4,5,6]
+const bsTree = new CBTInserter(new TreeNode(arr.shift(),null,null))
+arr.forEach(item=>{
+    bsTree.insert(item)
+})
+console.log(bsTree);
+console.log(countNodes(bsTree.get_root()));
--- algorithm/2023-6-15/搜索旋转排序数组.js
+++ algorithm/2023-6-15/搜索旋转排序数组.js
@@ -1,1 +1,29 @@
-// 等待...
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    if(!nums || nums.length === 0) {
+        return -1;
+    }
+    let l = 0, r = nums.length - 1;
+    while(l <= r ) {
+        let mid = l + ((r - l) >> 1)
+        if(target === nums[mid]) return mid;
+        if(nums[mid] > nums[r]) {
+            if(target <= nums[r] || target > nums[r] && target >  nums[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        } else {
+            if(target > nums[mid] && target <= nums[r]) {
+                l = mid + 1;
+            } else{
+                r = mid - 1;
+            }
+        }
+    }
+    return -1;
+};
