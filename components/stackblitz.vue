4f1ae849fc06da08fa1a590d7562fb2567b26c73
zhaohao <601095001@qq.com>
2023-06-29 15:28:10 +0800

    feat: 算法

--- components/stackblitz.vue
+++ components/stackblitz.vue
@@ -1,34 +1,35 @@
 <template>
-    <div style="height: 100%">
-        <div :id="id"></div>
-    </div>
+  <div style="height: 100%">
+    <div :id="id"></div>
+  </div>
 </template>
 <script>
 import sdk from '@stackblitz/sdk'
 
 export default {
-    name:"stackblitz",
-    data(){
-      return {
-          id:"stackblitz-"+Date.now()
-      }
-    },
-    props:{
-        projectId:String,
-        embedOptions:{
-            default:{
-                forceEmbedLayout: true,
-                openFile: 'index.js',
-                view:"editor"
-            }
-        }
-    },
-    mounted() {
-        sdk.embedProjectId(
-            this.id,
-            this.projectId,
-            this.embedOptions
-        );
+  name: "stackblitz",
+  data() {
+    return {
+      id: "stackblitz-" + Date.now()
     }
+  },
+  props: {
+    projectId: String,
+    embedOptions: {
+      default: {
+        forceEmbedLayout: true,
+        openFile: 'index.js',
+        view: "editor",
+        height: 600,
+      }
+    }
+  },
+  mounted() {
+    sdk.embedProjectId(
+        this.id,
+        this.projectId,
+        this.embedOptions
+    );
+  }
 }
 </script>
