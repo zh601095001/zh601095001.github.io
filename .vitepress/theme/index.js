7cae397eb5117520531d6fc3d8986e32fabeda48
ZH <601095001@qq.com>
2023-12-24 15:57:48 +0800

    feat: 增加图片点击放大功能

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -21,44 +21,6 @@ export default {
         })
     },
     async enhanceApp({app, router, siteData}) {
-
-        document.addEventListener('click', function (event) {
-            // 检查触发事件的元素是否是 img 标签或其子元素
-            var targetElement = event.target;
-
-            // 如果是 img 标签或其子元素
-            if (targetElement.tagName === 'IMG') {
-                // 在这里执行你想要的操作
-                const image = new Image();
-                image.src = targetElement.src;
-                image.onload = () => {
-                    // 创建弹出层
-                    const previewContainer = document.createElement('div');
-                    previewContainer.style.position = 'fixed';
-                    previewContainer.style.top = 0;
-                    previewContainer.style.bottom = 0;
-                    previewContainer.style.left = 0;
-                    previewContainer.style.right = 0;
-                    previewContainer.style.backgroundColor = 'rgba(0,0,0,0.8)';
-                    previewContainer.style.display = 'flex';
-                    previewContainer.style.justifyContent = 'center';
-                    previewContainer.style.alignItems = 'center';
-                    previewContainer.style.zIndex = 99999
-                    document.body.appendChild(previewContainer);
-                    // 在弹出层中添加图片
-                    const previewImage = document.createElement('img');
-                    previewImage.src = targetElement.src;
-                    previewImage.style.maxWidth = '80%';
-                    previewImage.style.maxHeight = '80%';
-                    previewContainer.appendChild(previewImage);
-                    // 点击弹出层，关闭预览
-                    previewContainer.addEventListener('click', (e) => {
-                        e.stopPropagation()
-                        document.body.removeChild(previewContainer);
-                    });
-                };
-            }
-        });
         app.component('AlgorithmLevel', AlgorithmLevel)
         app.component('Stackblitz', Stackblitz)
         app.component('Rank', Rank)
@@ -71,6 +33,44 @@ export default {
         if (!import.meta.env.SSR) {
             const plugin = await import('vue3-video-play')
             app.use(plugin)
+
+            document.addEventListener('click', function (event) {
+                // 检查触发事件的元素是否是 img 标签或其子元素
+                var targetElement = event.target;
+
+                // 如果是 img 标签或其子元素
+                if (targetElement.tagName === 'IMG') {
+                    // 在这里执行你想要的操作
+                    const image = new Image();
+                    image.src = targetElement.src;
+                    image.onload = () => {
+                        // 创建弹出层
+                        const previewContainer = document.createElement('div');
+                        previewContainer.style.position = 'fixed';
+                        previewContainer.style.top = 0;
+                        previewContainer.style.bottom = 0;
+                        previewContainer.style.left = 0;
+                        previewContainer.style.right = 0;
+                        previewContainer.style.backgroundColor = 'rgba(0,0,0,0.8)';
+                        previewContainer.style.display = 'flex';
+                        previewContainer.style.justifyContent = 'center';
+                        previewContainer.style.alignItems = 'center';
+                        previewContainer.style.zIndex = 99999
+                        document.body.appendChild(previewContainer);
+                        // 在弹出层中添加图片
+                        const previewImage = document.createElement('img');
+                        previewImage.src = targetElement.src;
+                        previewImage.style.maxWidth = '80%';
+                        previewImage.style.maxHeight = '80%';
+                        previewContainer.appendChild(previewImage);
+                        // 点击弹出层，关闭预览
+                        previewContainer.addEventListener('click', (e) => {
+                            e.stopPropagation()
+                            document.body.removeChild(previewContainer);
+                        });
+                    };
+                }
+            });
         }
     }
 }
